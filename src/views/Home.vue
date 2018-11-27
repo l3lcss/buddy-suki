aca9d5cfd52fc35bab5fcf9fddd5642a970bf688
princel3lcS <princeboy__@hotmail.com>
2018-11-27 11:57:59 +0700

    debug random buddy

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
-    Home
-    <button @click="findBuddy()"> find buddy </button>
-    <button @click="runScript()"> runScript </button>
+    {{ ownDetails }}<br>
+    {{ buddyDetails }}
+    <button @click="findBuddy()" v-if="!hasBuddy"> find buddy </button>
   </div>
 </template>
 
@@ -10,48 +10,58 @@
 import { db } from '../config/FirebaseConfig.js'
 export default {
   name: 'Home',
+  data () {
+    return {
+      hasBuddy: false,
+      ownDetails: {},
+      buddyDetails: {},
+      numOfPeopleWithoutBudder: 0
+    }
+  },
   props: ['profile'],
   methods: {
-    // async runScript () {
-    // let res = await db.ref('people_without_buddy').once('value')
-    // res.forEach(data => {
-    //   let val = data.val()
-    //   db.ref(`employees/${val.id}`).set(val)
-    // })
-    // },
     async findBuddy () {
-      let randomBuddy = await this.getBuddyRandom()
-      let ownKey = await this.findKey('employees', 'email', this.profile.email)
-
-      await db.ref(`employees/${ownKey}`).update({
-        buddy: randomBuddy
+      do {
+        await this.getBuddyRandom()
+      } while (this.buddyDetails.id === this.ownDetails.id)
+      await db.ref(`employees/${this.ownDetails.id}`).update({
+        buddy: this.buddyDetails.id
       })
-      await db.ref(`employees/${randomBuddy}`).update({
-        budder: ownKey
+      await db.ref(`employees/${this.buddyDetails.id}`).update({
+        budder: this.ownDetails.id
       })
-      await db.ref(`people_without_budder/${randomBuddy}`).remove()
+      await db.ref(`people_without_budder/${this.buddyDetails.id}`).remove()
     },
     async getBuddyRandom () {
       let res = await db.ref('people_without_budder').once('value')
       let max = res.numChildren()
-      let numRandom = Math.floor(Math.random() * (max - 0)) + 0
+      this.numOfPeopleWithoutBudder = max
+      let numRandom = Math.floor(Math.random() * max) + 1
 
       let withoutBuddyRef = await db.ref('people_without_budder').once('value')
       let withoutBuddyData = withoutBuddyRef.val()
       let i = 1
-      let keyBuddy = ''
       for (const key in withoutBuddyData) {
         if (i === numRandom) {
-          keyBuddy = key
+          this.buddyDetails = withoutBuddyData[key]
           break
         }
         i = i + 1
       }
-      return keyBuddy
     },
     async findKey (fKey, sKey, val) {
       let res = await db.ref(fKey).orderByChild(sKey).equalTo(val).once('value')
       return Object.keys(res.val()).shift()
+    }
+  },
+  async mounted () {
+    let key = await this.findKey('employees', 'email', this.profile.email)
+    let ref = await db.ref(`employees/${key}`).once('value')
+    this.ownDetails = ref.val()
+    if (this.ownDetails.buddy) {
+      this.hasBuddy = true
+      let res = await db.ref(`employees/${this.ownDetails.buddy}`).once('value')
+      this.buddyDetails = res.val()
     }
   }
 }
